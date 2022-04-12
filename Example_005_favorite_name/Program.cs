8b06693be8dcebf62e57534ac3c79a25e2f9b4b8
Vladimir <_Vladimir210@yandex.ru>
2022-04-12 12:15:02 +0300

    ToLower commit

---
+++ Example_005_favorite_name/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What is your name?");
+string username = Console.ReadLine();
+if (username.ToLower() =="mari")
+{
+    Console.WriteLine ("I'm so happy to see you!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
