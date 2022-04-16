a897810b13060f0ab977e2b73f73a0c060e80d1e
Vladimir <_Vladimir210@yandex.ru>
2022-04-16 23:19:57 +0300

    Поиск индекса в массиве по его содержанию

--- Exampel_009/Program.cs
+++ Exampel_009/Program.cs
@@ -1,21 +1,13 @@
-﻿int Max( int arg1, int arg2, int arg3 )
+﻿int [] array = {12, 1, 2, 3, 43, 56, 5, 6, 7, 8, 9, 12};
+int n = array.Length;
+int find = 12;
+int index = 0;
+while (index<n)
 {
-     int result = arg1;
-     if(arg2 > result) result = arg2;
-     if(arg3 > result) result = arg3;  
-     return result;
+    if (array[index]== find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index +1; // index ++
 }
-
-int[] array = { 100, 12, 45, 65, 73, 89, 54, 156, 34,};
-int hide = array [2];
-array [2] = 0;
-
-Console.WriteLine (array[2]);
-
-
-int max = Max (Max(array[0],array[1],array[2]),
-      Max(array[3],array[4],array[5]),
-      Max(array[6],array[7],array[8])
-    );
-Console.WriteLine (max);
-Console.WriteLine (hide);
